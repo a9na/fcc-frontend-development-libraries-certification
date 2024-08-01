5614138714473e90c80c4401c02461cd4231dfa9
Ana Novković <anaiscoding@gmail.com>
2024-08-01 14:30:51 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './Calculator';
+import './styles.css';
+
+const App = () => {
+  return (
+    <div className="AppStyle">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
